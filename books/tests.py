b822dea283894e29140cfc45e855280819b0cf62
mcontrerasgarcia <miguel.contreras@yumminn.com>
2025-03-17 13:21:44 +0100

    AÃñadidos algunos test a la API

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from books.models import Book
+
+
+class BookAPITests(APITestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_username",
+            email="test@example.com",
+            password="testpassword",
+            role="editor"
+        )
+
+        self.token = str(RefreshToken.for_user(self.user).access_token)
+
+        self.book = Book.objects.create(
+            title="Test Book",
+            author=self.user,
+        )
+
+        self.url = reverse("book-list")
+
+    def test_create_book(self):
+        data = {
+            "title": "New Book",
+            "pages": [
+                {"number": 1, "content": "Content of page 1"},
+                {"number": 2, "content": "Content of page 2"}
+            ]
+        }
+        json_data = json.dumps(data)
+
+        response = self.client.post(self.url, json_data, content_type='application/json',
+                                    HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_list_books(self):
+        response = self.client.get(self.url, HTTP_AUTHORIZATION=f"Bearer {self.token}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_update_book(self):
+        data = {
+            "title": "Updated Book",
+            "pages": [
+                {"number": 1, "content": "Updated content of page 1"},
+                {"number": 2, "content": "Updated content of page 2"}
+            ]
+        }
+        json_data = json.dumps(data)
+        response = self.client.put(reverse("book-detail", args=[self.book.id]), json_data,
+                                   content_type='application/json', HTTP_AUTHORIZATION=f"Bearer {self.token}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Book")
+
+    def test_delete_book(self):
+        response = self.client.delete(reverse("book-detail", args=[self.book.id]),
+                                      HTTP_AUTHORIZATION=f"Bearer {self.token}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_unauthenticated_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,63 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+
+
+class UserAPITests(APITestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_user",
+            email="testuser@example.com",
+            password="testpassword",
+            role="reader"
+        )
+
+        self.login_url = reverse('token_obtain_pair')
+        self.refresh_url = reverse('token_refresh')
+        self.url = reverse('user-list')
+
+    def test_user_registration(self):
+        data = {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'newpassword'
+        }
+        json_data = json.dumps(data)
+
+        response = self.client.post(reverse('user-list'), json_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 2)
+
+    def test_user_login(self):
+        data = {
+            'username': 'testuser@example.com',
+            'password': 'testpassword'
+        }
+        json_data = json.dumps(data)
+
+        response = self.client.post(self.login_url, json_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+
+    def test_user_refresh_token(self):
+        refresh_token = str(RefreshToken.for_user(self.user))
+        data = {
+            'refresh': refresh_token
+        }
+        response = self.client.post(self.refresh_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+
+    def test_unauthenticated_user_cannot_access(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_user_can_access_user_list(self):
+        token = str(RefreshToken.for_user(self.user).access_token)
+        response = self.client.get(self.url, HTTP_AUTHORIZATION=f'Bearer {token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
